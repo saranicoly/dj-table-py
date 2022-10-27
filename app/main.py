c9f7f9c960db29922e24e55ef4d1b83bc5c966f0
Sara Lins <sara.lins@luizalabs.com>
2022-10-27 14:06:18 -0300

    chore: wip

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 import os
 import pygame
 import pygame_menu
+import os, sys
+from fcntl import ioctl
+
+# ioctl commands defined at the pci driver
+RD_SWITCHES   = 24929
+RD_PBUTTONS   = 24930
+WR_L_DISPLAY  = 24931
+WR_R_DISPLAY  = 24932
+WR_RED_LEDS   = 24933
+WR_GREEN_LEDS = 24934
 
 pygame.init()
 pygame.mixer.init()
@@ -32,8 +42,21 @@ def toggle_music(state, args):
     else:
         pygame.mixer.Channel(music_number).stop()
 
-def testing():
-    toggle_music(state=1, args=(1,))
+if len(sys.argv) < 2:
+    print("Error: expected more command line arguments")
+    print("Syntax: %s </dev/device_file>"%sys.argv[0])
+    exit(1)
+
+fd = os.open(sys.argv[1], os.O_RDWR)
+
+def ativa_musica(music_number):
+    ioctl(fd, RD_SWITCHES)
+    red = os.read(fd, 4); # read 4 bytes and store in red var
+    red = int.from_bytes(red, 'little')
+
+    if (red % 4 == 0):
+        toggle_music(state=1, args=[music_number])
+
 
 load_songs()
 
@@ -47,7 +70,7 @@ for i in range(len(song_list)):
         switch_height=0.8,
         switch_margin=(12, 0),
         toggleswitch_id=str(i),
-        onchange=testing(),
+        onchange=ativa_musica(i),
         args=(i,),
         align=pygame_menu.locals.ALIGN_RIGHT,
     )
